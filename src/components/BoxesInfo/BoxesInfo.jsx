61102508f05d57d77ad7c39b743a68fcddbbd1e3
Madhav Bansal <madhavbansal2005@gmail.com>
2024-09-06 12:39:31 +0530

    added emoji

--- src/components/BoxesInfo/BoxesInfo.jsx
+++ src/components/BoxesInfo/BoxesInfo.jsx
@@ -6,32 +6,32 @@ const BoxesInfo = () => {
             <h1>What is Agrow ?</h1>
             <div class="container-boxes">
                 <div class="service-box1">
-                    <div class="icon">📦</div>
+                    <div class="icon">🌾</div>
                     <h3>Personalized Crop Recommendations</h3>
                     <p>Analyzes land, soil, climate, and demand for optimized crop suggestions.</p>
                 </div>
                 <div class="service-box">
-                    <div class="icon">👥</div>
+                    <div class="icon">⏱️</div>
                     <h3>Real-Time Supply Tracking</h3>
                     <p>Tracks farmer crop updates to prevent oversupply and balance demand.</p>
                 </div>
                 <div class="service-box1">
-                    <div class="icon">🌱</div>
+                    <div class="icon">💰</div>
                     <h3>MSP Assurance</h3>
                     <p>Ensures crops align with MSP policies for guaranteed returns.</p>
                 </div>
                 <div class="service-box">
-                    <div class="icon">🧬</div>
+                    <div class="icon">📊</div>
                     <h3>Aggrow Score</h3>
                     <p>Ranks crops based on suitability and profitability factors.</p>
                 </div>
                 <div class="service-box1">
-                    <div class="icon">🔬</div>
+                    <div class="icon"> 📈</div>
                     <h3>Market Demand Insights</h3>
                     <p>Provides trends and profitability data for better crop choices.</p>
                 </div>
                 <div class="service-box">
-                    <div class="icon">🏢</div>
+                    <div class="icon">🖥️</div>
                     <h3>Simple Interface</h3>
                     <p>Easy navigation with crop category filters and detailed reports.</p>
                 </div>
